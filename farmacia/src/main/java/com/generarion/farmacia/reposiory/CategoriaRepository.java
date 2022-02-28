1d18bc036a5428904a42a78da3a23b9dcec29511
dennerv <denner.vieira@live.com>
2022-02-28 15:21:37 -0300

    Projeto Farmácia

---
+++ farmacia/src/main/java/com/generarion/farmacia/reposiory/CategoriaRepository.java
@@ -0,0 +1,17 @@
+package com.generarion.farmacia.reposiory;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generarion.farmacia.model.Categoria;
+
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List <Categoria> findAllByNomeContainingIgnoreCase(String nome);
+
+}
